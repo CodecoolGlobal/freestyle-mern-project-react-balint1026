4a8ccbf475953885bc364f997597ec145b723693
Apolder <98808596+Apolderr@users.noreply.github.com>
2023-09-05 14:20:10 +0200

    Add get patch endpoints

--- server/server.js
+++ server/server.js
@@ -8,9 +8,37 @@ const app = express();
 dotenv.config();
 
 mongoose.set("strictQuery", false);
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8u3exxo.mongodb.net/project`);
+mongoose.connect(`mongodb+srv://Apolder:CKMKKp12rQZGyaxa@cluster.8u3exxo.mongodb.net/project?retryWrites=true&w=majority`);
 
 app.use(express.json());
+
+//User Endpoint
+
+//get all users
+app.get('/api/users', (req, res) => {
+    console.log("happeb");
+    User.find({}).sort({ username: 1 }).then(users => res.status(200).json(users));
+    
+})
+
+//-- get a user
+app.get('/api/users/:id', (req, res) => {
+    User.findById(req.query.id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(() => {
+            res.status(500).json({ error: "Could not fetch the document" })
+        });
+});
+//update user
+app.patch('/api/users/:id', (req, res) => {
+    User.findByIdAndUpdate(req.query.id, { $set: req.body.user })
+        .then((user) => res.status(200).json(user))
+        .catch(() => {
+            res.status(500).json({ error: "Could not update the document" })
+        });
+});
 
 //
 
